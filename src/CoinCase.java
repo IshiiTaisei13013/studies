fb16e1184ec3961b61fb40ebee62b5bc76a3e6be
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-12 15:07:35 +0900

    [update] 7-4終了

---
+++ src/CoinCase.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoinCase {
+
+    private int[] Coins = new int[6];
+    private Map<Integer,Integer> map = new HashMap<>();
+
+    public CoinCase(){
+        //最初にコインを定義
+        map.put(0,1);
+        map.put(1,5);
+        map.put(2,10);
+        map.put(3,50);
+        map.put(4,100);
+        map.put(5,500);
+    }
+    public void AddCoin(int order,int num){
+        Coins[order] += num;
+    }
+
+    public void ShowCoins(){
+        for(int i=0;i<Coins.length;i++){
+            System.out.println(map.get(i) + ":" + Coins[i]);
+        }
+    }
+
+    public void GetCount(int order){
+        System.out.println(Coins[order]);
+//        return Coins[order];
+    }
+
+    public void GetAmount(){
+        int total = 0;
+
+        for(int i = 0;i<Coins.length;i++){
+            while(Coins[i] > 0){
+                total += map.get(i);
+                Coins[i]--;
+            }
+        }
+        System.out.println(total);
+//        return total;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
 public class main {
-    public static void main(){
+    public static void main(String args[]){
+
+        CoinCase Yen = new CoinCase();
+
+        Yen.AddCoin(1,4);
+        Yen.AddCoin(2,3);
+        Yen.AddCoin(5,1);
+        Yen.AddCoin(0,2);
+
+        Yen.ShowCoins();
+
+        Yen.GetCount(1);
+
+
+        Yen.GetAmount();
+
 
     }
 }
