87f5c8b6b6f7b14e5cba75417c664bdaef999960
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-12 16:31:07 +0900

    [update] コメント追加

--- src/CoinCase.java
+++ src/CoinCase.java
@@ -6,9 +6,9 @@ public class CoinCase {
     private int[] Coins = new int[6];
     private static final Map<Integer,Integer> map;
 
+    //最初にコインを定義
     static {
         map = new HashMap<>();
-        //最初にコインを定義
         map.put(0,1);
         map.put(1,5);
         map.put(2,10);
@@ -16,16 +16,20 @@ public class CoinCase {
         map.put(4,100);
         map.put(5,500);
     }
+
+    //コインを追加する関数
     public void AddCoin(int order,int num){
         Coins[order] += num;
     }
 
+    //コインを表示する関数
     public void ShowCoins(){
         for(int i=0;i<Coins.length;i++){
             System.out.println(map.get(i) + ":" + Coins[i]);
         }
     }
 
+    //コイン全部を表示する関数
     public void GetCount(int order){
         System.out.println(Coins[order]);
 //        return Coins[order];
