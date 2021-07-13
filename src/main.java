180c17ba73bfc157dfaf7c18f3d317a8287d2721
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-13 12:29:27 +0900

    [update] コメント追加

--- src/main.java
+++ src/main.java
@@ -1,18 +1,26 @@
 public class main {
     public static void main(String args[]){
 
+        //宣言
         CoinCase Yen = new CoinCase();
 
+        //コインを追加
         Yen.AddCoin(1,4);
         Yen.AddCoin(5,3);
         Yen.AddCoin(500,1);
         Yen.AddCoin(100,2);
+        Yen.AddCoin(50,2);
 
+        //コインケースを表示
         Yen.ShowCoins();
 
+        //合計枚数を表示
         Yen.GetCount();
+
+        //コインごとの内訳金額を表示
         Yen.GetDetail();
 
+        //総額を表示
         Yen.GetAmount();
 
 
