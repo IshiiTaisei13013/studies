f1840f6042e9f70ed3d2b0678fa6979a2352b9ad
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-14 12:16:43 +0900

    [update] Playerを作成

---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public class Player extends Creature {
+
+    Player(int pos_y,int pos_x,String name,int hp,int mp,int atk,int def,int spd){
+        super(pos_y,pos_x,name,hp,mp,atk,def,spd);
+    }
+
+}
