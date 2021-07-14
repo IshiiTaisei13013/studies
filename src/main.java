ef18b6ee16c591cedd17c446dbc8a6b96e27b511
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-14 12:17:30 +0900

    [update] mainの修正

--- src/main.java
+++ src/main.java
@@ -1,6 +1,23 @@
-public class main {
-    public static void main(String args[]){
+import java.util.Scanner;
 
+
+public class main {
+    public static void main(String... args){
+
+        String name = "Hero";
+        WorldMap map = new WorldMap(20,20);
+        Player player = new Player(0,0,name,10,10,5,5,4);
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(player.dead){
+            String input_text = scanner.nextLine();
+
+            player.move(KeyBindings.inputToCommand(input_text));
+
+        }
 
     }
+
+
 }
