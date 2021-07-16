bb7158bd45d829f245fd90730715ba8be327b6c6
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-16 12:24:50 +0900

    [update] String の練習

--- src/main.java
+++ src/main.java
@@ -1,6 +1,27 @@
 public class main {
     public static void main(String args[]){
 
+        String a = "aa";
+        String b = "bb";
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(a);
+        builder.append(b);
+
+        String str = builder.toString();
+        System.out.println(str);
+
+        String sentence = "this is a pen.";
+        String[] words = sentence.split(" ");
+
+        for(String word : words) {
+            System.out.println(word);
+        }
+
+
+
+
 
     }
 }
