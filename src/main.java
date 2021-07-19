cffc5288d64ded5a183b6ecc92432da09cfd5949
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-19 13:39:38 +0900

    [update] propertiesのサンプルを記述

--- src/main.java
+++ src/main.java
@@ -1,5 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class main {
     public static void main(String args[]){
+        Path path = Paths.get("mail.propaties");
+
+        try(BufferedReader reader = Files.newBufferedReader(
+                path, StandardCharsets.UTF_8
+        )){
+            Properties properties = new Properties();
+            properties.load(reader);
+
+            String address = properties.getProperty("system.mail.address");
+
+            System.out.println(address);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
